04e30317a4950a3dc41f7eaee17eb1c8d11b3ea0
saraashraf94 <sara.mady9440@yahoo.com>
2018-03-08 21:50:28 +0200

    sara3

--- Library/catalog/urls.py
+++ Library/catalog/urls.py
@@ -6,10 +6,11 @@ from . import views
 
 
 urlpatterns = [
-path('', views.index, name='index'),
-path('books/', views.BookListView.as_view(), name='books'),
-path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
-path('writers/', views.WriterListView.as_view(), name='writers'),
-path('writer/<int:pk>', views.WriterDetailView.as_view(), name='writer-detail')
-
+#path('index/', views.index, name='index'),
+    path('', views.index, name='index'),
+     path('signup', views.signup, name='signup'),
+     path('books/', views.BookListView.as_view(), name='books'),
+     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+     path('writers/', views.WriterListView.as_view(), name='writers'),
+     path('writer/<int:pk>', views.WriterDetailView.as_view(), name='author-detail')
 ]
